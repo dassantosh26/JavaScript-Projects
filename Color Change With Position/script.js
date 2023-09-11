bba792baad0f29b5d4e8709c64995beaa5b26d38
Santosh Kumar Das <santoshdas.mits@gmail.com>
2023-09-11 22:41:17 +0530

    Another Project

---
+++ Color Change With Position/script.js
@@ -0,0 +1,36 @@
+const rectangle = document.querySelector("#center");
+rectangle.addEventListener("mousemove", function (details) {
+  let rectangleLocation = rectangle.getBoundingClientRect();
+  let insideRectaVal = details.clientX - rectangleLocation.left;
+  if (insideRectaVal < rectangleLocation.width / 2) {
+    let redColor = gsap.utils.mapRange(
+      0,
+      rectangleLocation.width / 2,
+      255,
+      0,
+      insideRectaVal
+    );
+    gsap.to(rectangle, {
+      backgroundColor: `rgb(${redColor},0,0)`,
+      ease: Power4,
+    });
+  } else {
+    let blueColor = gsap.utils.mapRange(
+      rectangleLocation.width / 2,
+      rectangleLocation.width,
+      0,
+      255,
+      insideRectaVal
+    );
+    gsap.to(rectangle, {
+      backgroundColor: `rgb(0,0,${blueColor})`,
+      ease: Power4,
+    });
+  }
+});
+
+rectangle.addEventListener("mouseleave", function () {
+  gsap.to(rectangle, {
+    backgroundColor: "white",
+  });
+});
